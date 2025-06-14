f27df86634aba26939790297002d37d025ad6ac6
vashrom <vas-hrom@ukr.net>
2025-06-14 13:50:27 +0300

    Add a math function

--- Source.cpp
+++ Source.cpp
@@ -7,12 +7,17 @@ void newFunction()
 	cout << "Wow. It is work now!!!";
 }
 
+void math(int a, int b)
+{
+	cout << a + b;
+}
+
 void main() {
 	cout << "Repo working!!!";
 	
 	cout << "We are connected!!!";
 
 	newFunction();
-
+	math(11, 11);
 
 }
