602648b6fe7acd3cccc8e7674e08097ccbcd891c
vashrom <vas-hrom@ukr.net>
2025-06-14 13:45:20 +0300

    Add a new function

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+void newFunction()
+{
+	cout << "Wow. It is work now!!!";
+}
+
+void main() {
+	cout << "Repo working!!!";
+	
+	cout << "We are connected!!!";
+
+	newFunction();
+
+
+}
